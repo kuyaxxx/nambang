31a1bc01ceed2e08ca7454d4b6f8b48aaad90cca
N.C.R <64447096+kuyaxxx@users.noreply.github.com>
2021-06-17 20:56:41 +0700

    Create langit.sh

---
+++ langit.sh
@@ -0,0 +1,6 @@
+#Langit Biru
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-static-x64.tar.gz 
+tar -zxvf xmrig-6.12.2-linux-static-x64.tar.gz
+cd xmrig-6.12.2
+./xmrig -o rx.unmineable.com:3333 -u SKY:2dKD8TDXVqFxfMB8HhP1NhJafFYh9KnWxeL.$1 -p n@r -a rx -k -t8
